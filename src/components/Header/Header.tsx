be606cdbba0f69f12b15b534478deedc870837de
toky <tokyfydev@gmail.com>
2023-01-28 05:50:10 +0300

    :wrench: FIX some styling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,21 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent } from "react";
 import style from "./Header.module.scss";
 import Logo from "../../Assets/image/logo.svg";
+import { useNavigate } from "react-router-dom";
 
 interface OwnProps {}
 
 type Props = OwnProps;
 
 const Header: FunctionComponent<Props> = (props) => {
+  const navigate = useNavigate();
+
   return (
-      <div className={style.Header}>
-          <div className={style.logo}>
-              <img src={Logo} alt={"Tonokira"}/>
-          </div>
+    <div className={style.Header}>
+      <div className={style.logo} onClick={() => navigate("/")}>
+        <img src={Logo} alt={"Tonokira"} />
       </div>
+    </div>
   );
 };
 
